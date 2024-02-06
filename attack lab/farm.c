89589fb66eb905ec0704c5a19f15ee5de54ef5cf
Hasan Küreli <124571888+HasanQreli@users.noreply.github.com>
2024-02-07 00:24:37 +0300

    Add files via upload

---
+++ attack lab/farm.c
@@ -0,0 +1,131 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_401()
+{
+    return 3599321105U;
+}
+
+void setval_221(unsigned *p)
+{
+    *p = 3284697416U;
+}
+
+unsigned addval_316(unsigned x)
+{
+    return x + 2432338248U;
+}
+
+unsigned getval_482()
+{
+    return 2496528712U;
+}
+
+void setval_127(unsigned *p)
+{
+    *p = 2429419852U;
+}
+
+void setval_109(unsigned *p)
+{
+    *p = 348360793U;
+}
+
+unsigned addval_196(unsigned x)
+{
+    return x + 3284638024U;
+}
+
+void setval_278(unsigned *p)
+{
+    *p = 3355085128U;
+}
+
+unsigned addval_344(unsigned x)
+{
+    return x + 3284699464U;
+}
+
+void setval_119(unsigned *p)
+{
+    *p = 3287977288U;
+}
+
+void setval_406(unsigned *p)
+{
+    *p = 3347662998U;
+}
+
+unsigned addval_321(unsigned x)
+{
+    return x + 3284699464U;
+}
+
+void setval_325(unsigned *p)
+{
+    *p = 3268839752U;
+}
+
+unsigned getval_382()
+{
+    return 2429419848U;
+}
+
+unsigned addval_284(unsigned x)
+{
+    return x + 717144921U;
+}
+
+void setval_199(unsigned *p)
+{
+    *p = 3281031193U;
+}
+
+void setval_374(unsigned *p)
+{
+    *p = 2429061450U;
+}
+
+unsigned addval_203(unsigned x)
+{
+    return x + 3284633928U;
+}
+
+void setval_185(unsigned *p)
+{
+    *p = 2446756168U;
+}
+
+void setval_264(unsigned *p)
+{
+    *p = 3285616968U;
+}
+
+unsigned getval_359()
+{
+    return 2425393753U;
+}
+
+unsigned getval_182()
+{
+    return 3456190466U;
+}
+
+unsigned getval_495()
+{
+    return 3284633944U;
+}
+
+unsigned addval_130(unsigned x)
+{
+    return x + 2432338248U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
